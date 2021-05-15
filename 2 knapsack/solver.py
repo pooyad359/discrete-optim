a4fe22c3d52a844f1810c360e19f5870082260aa
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-05-15 21:44:44 +0800

    added local search

--- 2 knapsack/solver.py
+++ 2 knapsack/solver.py
@@ -4,7 +4,11 @@
 from collections import namedtuple
 import numpy as np
 from numba import jit
-Item = namedtuple("Item", ['index', 'value', 'weight'])
+from tqdm.auto import tqdm
+
+Item = namedtuple("Item", ["index", "value", "weight"])
+N_TRIALS = 100
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -15,25 +19,30 @@ def solve_it(input_data):
     n, cap = data.pop(0)
     n, cap = int(n), float(cap)
     data = np.array(data, dtype=float)
-    values = data[:,0]
-    weights = data[:,1]
+    values = data[:, 0]
+    weights = data[:, 1]
 
     # Optimisation
-    if n>400:
-        print('### Greedy Algo ###')
+    if n > 400:
+        print("### Greedy Algo ###")
         selection, weight, count, spare = greedy_dens(values, weights, cap)
-
+        for i in tqdm(range(N_TRIALS)):
+            selection, weight, count, spare = local_search(
+                values, weights, cap, selection
+            )
+            # solution = output[0]
     # elif n>:
     #     selection, weight, count,spare = DynP(values, weights, cap)
     else:
-        print('### Dynamic Programming ###')
+        print("### Dynamic Programming ###")
         selection, weight, count, spare = DynPjit(values, weights, cap)
     value = np.sum(selection * values)
-    
+
     # prepare the solution in the specified output format
-    output_data = str(value.astype(int)) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, selection.astype(int)))
+    output_data = str(value.astype(int)) + " " + str(0) + "\n"
+    output_data += " ".join(map(str, selection.astype(int)))
     return output_data
+
 
 def greedy_dens(values, weights, capacity):
     n = len(values)
@@ -54,6 +63,38 @@ def greedy_dens(values, weights, capacity):
         np.sum(selection).astype(int),
         spare,
     )
+
+
+def local_search(values, weights, capacity, solution):
+    n = len(values)
+    init_value = np.dot(values, solution)
+    densities = values / weights
+    idx = np.argsort(densities)
+    idx_ones = np.nonzero(solution)[0]
+    switched = np.random.choice(idx_ones)
+    selection = solution.copy()
+    selection[switched] = 0
+    # print(switched, idx_ones)
+    for i in reversed(idx):
+        if i == switched or selection[i]:
+            continue
+        selection[i] = 1
+        if np.dot(selection, weights) > capacity:
+            selection[i] = 0
+
+    final_value = np.dot(selection, values)
+    if final_value < init_value:
+        selection = solution.copy()
+        final_value = init_value
+    final_weight = np.dot(selection, weights)
+    spare = capacity - final_weight
+    return (
+        selection,
+        final_weight,
+        selection.sum(),
+        spare,
+    )
+
 
 def DynP(values, weights, capacity):
     rows, cols = int(capacity) + 1, len(values) + 1
@@ -88,26 +129,34 @@ def DynP(values, weights, capacity):
         spare,
     )
 
-@jit
-def DynPjit(values, weights, capacity):
-    rows, cols = int(capacity) + 1, len(values) + 1
-    mat = np.zeros((rows, cols))
-    for row in range(rows):
-        for col in range(1, cols):
 
-            i = col - 1
-            wi = int(weights[i])
-            vi = int(values[i])
+@jit(
+    parallel=False,
+)
+def DynPjit(values, weights, capacity):
+    weights = np.int32(weights)
+    values = np.int32(values)
+    capacity = np.int32(capacity)
+    nitems = np.shape(values)[0]
+    rows = np.int32(capacity) + 1
+    cols = np.int32(nitems + 1)
+    mat = np.zeros((rows, cols))
+    for row in np.arange(rows, dtype=np.int32):
+        for col in np.arange(1, cols, dtype=np.int32):
+
+            i = np.int32(col - 1)
+            wi = np.int32(weights[i])
+            vi = np.int32(values[i])
             if row < wi:
                 mat[row, col] = mat[row, col - 1]
             else:
                 mat[row, col] = max(mat[row, col - 1], vi + mat[row - wi, col - 1])
-    selection = np.zeros(len(values), dtype=int)
+    selection = np.zeros(nitems, dtype=np.int32)
     row = rows - 1
-    for c in range(len(values), 0, -1):
+    for c in range(nitems, 0, -1):
         if mat[row, c] != mat[row, c - 1]:
             selection[c - 1] = 1
-            row = row - int(weights[c - 1])
+            row = row - np.int32(weights[c - 1])
 
     #     return mat[-1, -1], selection
     final_weight = np.sum(selection * weights)
@@ -116,19 +165,22 @@ def DynPjit(values, weights, capacity):
     #     print(f"***Warning: {np.sum(selection * values)} =/= {mat[-1,-1]}")
     spare = capacity - final_weight
     return (
-        selection.astype(int),
-        np.sum(selection * weights).astype(int),
-        np.sum(selection).astype(int),
+        selection.astype(np.int8),
+        np.dot(selection, weights),
+        np.sum(selection, dtype=np.int32),
         spare,
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        with open(file_location, 'r') as input_data_file:
+        with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
     else:
-        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
-
+        print(
+            "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)"
+        )
