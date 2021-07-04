375c9e114d92144aa7abf57bacd21cb3aac15d0b
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-07-04 16:23:34 +0800

    updated solver

--- 5 facility/solver.py
+++ 5 facility/solver.py
@@ -3,75 +3,94 @@
 
 from collections import namedtuple
 import math
+from os.path import join
+import os
+from calc import length, total_cost
+from algorithms import greedy, local_search, random_allocation
 
-Point = namedtuple("Point", ['x', 'y'])
-Facility = namedtuple("Facility", ['index', 'setup_cost', 'capacity', 'location'])
-Customer = namedtuple("Customer", ['index', 'demand', 'location'])
+ls = os.listdir
 
-def length(point1, point2):
-    return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+Point = namedtuple("Point", ["x", "y"])
+Facility = namedtuple("Facility", ["index", "setup_cost", "capacity", "location"])
+Customer = namedtuple("Customer", ["index", "demand", "location"])
+
+
+def load_data(file, prefix="data"):
+    with open(join(prefix, file)) as fp:
+        content = fp.read()
+    return parse_input(content)
+
+
+def parse_input(input_data):
+    lines = input_data.split("\n")
+
+    parts = lines[0].split()
+    facility_count = int(parts[0])
+    customer_count = int(parts[1])
+
+    facilities = []
+    for i in range(1, facility_count + 1):
+        parts = lines[i].split()
+        facilities.append(
+            Facility(
+                index=i - 1,
+                setup_cost=float(parts[0]),
+                capacity=int(parts[1]),
+                location=Point(float(parts[2]), float(parts[3])),
+            )
+        )
+
+    customers = []
+    for i in range(facility_count + 1, facility_count + 1 + customer_count):
+        parts = lines[i].split()
+        customers.append(
+            Customer(
+                index=i - 1 - facility_count,
+                demand=int(parts[0]),
+                location=Point(float(parts[1]), float(parts[2])),
+            )
+        )
+
+    return customers, facilities
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
     # parse the input
-    lines = input_data.split('\n')
+    customers, facilities = parse_input(input_data)
+    facility_count = len(facilities)
+    customer_count = len(customers)
 
-    parts = lines[0].split()
-    facility_count = int(parts[0])
-    customer_count = int(parts[1])
-    
-    facilities = []
-    for i in range(1, facility_count+1):
-        parts = lines[i].split()
-        facilities.append(Facility(i-1, float(parts[0]), int(parts[1]), Point(float(parts[2]), float(parts[3])) ))
-
-    customers = []
-    for i in range(facility_count+1, facility_count+1+customer_count):
-        parts = lines[i].split()
-        customers.append(Customer(i-1-facility_count, int(parts[0]), Point(float(parts[1]), float(parts[2]))))
-
-    # build a trivial solution
-    # pack the facilities one by one until all the customers are served
-    solution = [-1]*len(customers)
-    capacity_remaining = [f.capacity for f in facilities]
-
-    facility_index = 0
-    for customer in customers:
-        if capacity_remaining[facility_index] >= customer.demand:
-            solution[customer.index] = facility_index
-            capacity_remaining[facility_index] -= customer.demand
-        else:
-            facility_index += 1
-            assert capacity_remaining[facility_index] >= customer.demand
-            solution[customer.index] = facility_index
-            capacity_remaining[facility_index] -= customer.demand
-
-    used = [0]*len(facilities)
-    for facility_index in solution:
-        used[facility_index] = 1
+    # Solution
+    mode = 0
+    if mode == 0:
+        solution = greedy(customers, facilities)
+        solution = local_search(solution, customers, facilities, True)
+    else:
+        solution = greedy(customers, facilities)
 
     # calculate the cost of the solution
-    obj = sum([f.setup_cost*used[f.index] for f in facilities])
-    for customer in customers:
-        obj += length(customer.location, facilities[solution[customer.index]].location)
 
+    obj = total_cost(solution, customers, facilities)
     # prepare the solution in the specified output format
-    output_data = '%.2f' % obj + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, solution))
+    output_data = "%.2f" % obj + " " + str(0) + "\n"
+    output_data += " ".join(map(str, solution))
 
     return output_data
 
 
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        with open(file_location, 'r') as input_data_file:
+        with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
     else:
-        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/fl_16_2)')
-
+        print(
+            "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/fl_16_2)"
+        )
