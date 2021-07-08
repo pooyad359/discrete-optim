c198e20eea708616d28b523855ba5bae384aed6d
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-07-08 18:27:23 +0800

    Added more metrics to problem analysis

--- 5 facility/vis.py
+++ 5 facility/vis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from calc import total_cost, validate
+from calc import min_facilities, total_cost, validate
 
 
 def view_problem(customers, facilities, figsize=(8, 8)):
@@ -45,10 +45,17 @@ def view_solution(solution, customers, facilities, figsize=(8, 8)):
 
 
 def problem_analysis(customers, facilities):
+    demands = [c.demand for c in customers]
+    caps = np.array([f.capacity for f in facilities])
     print(f"Customers: {len(customers)}")
     print(f"Facilities: {len(facilities)}")
+    print(f"Total Demand: {sum(demands)}")
+    print(f"Minimum = {demands.min()} \t Maximum = {demands.max}")
+    print(f"Total Capacity: {sum(caps)}")
+    print(f"Minimum = {caps.min()} \t Maximum = {caps.max}")
+    print(f"Minimum Facilities Required: {min_facilities(customers,facilities)}")
+
     # Capacity
-    caps = np.array([f.capacity for f in facilities])
     plt.figure()
     plt.hist(caps, 100)
     plt.title(f"Capacities: Average = {caps.mean():.1f} StD = {caps.std():.1f}")
@@ -60,10 +67,9 @@ def problem_analysis(customers, facilities):
     plt.title(f"Setup Cost: Average = {setup.mean():.1f} StD = {setup.std():.1f}")
 
     # Demand
-    demand = np.array([c.demand for c in customers])
     plt.figure()
-    plt.hist(demand, 100)
-    plt.title(f"Demand: Average = {demand.mean():.1f} StD = {demand.std():.1f}")
+    plt.hist(demands, 100)
+    plt.title(f"Demand: Average = {demands.mean():.1f} StD = {demands.std():.1f}")
 
     # Map
     view_problem(customers, facilities)
